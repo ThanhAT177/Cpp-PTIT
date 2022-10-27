73a72c7af7ac6c43ae060a8805ddb0d411f349c0
Nguyễn Quốc Thành <103249850+ThanhAT177@users.noreply.github.com>
2022-10-28 00:49:19 +0700

    Create CPP0317 - SỐ ĐẸP.cpp

---
+++ CPP0317 - SỐ ĐẸP.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+
+std::string check(std::string s) {
+	int l = s.length();
+	for(int i = 0; i <= l/2; ++i) {
+		if(s[i] % 2 == 1 || s[i] != s[l - 1 - i])
+			return "NO\n";
+	}
+	return "YES\n";
+}
+
+main() {
+	int T;
+	std::cin >> T;
+	while(T--) {
+		std::string x;
+		std::cin >> x;
+		std::cout << check(x);
+	}
+}
