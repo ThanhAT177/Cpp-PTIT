dadc222698f25b1032a4220fc7377d449026947d
Nguyễn Quốc Thành <103249850+ThanhAT177@users.noreply.github.com>
2022-10-28 00:57:13 +0700

    Create CPP0502 - CẤU TRÚC THÍ SINH.cpp

---
+++ CPP0502 - CẤU TRÚC THÍ SINH.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<iomanip>
+#include<string>
+using namespace std;
+
+struct ThiSinh {
+	string name;
+	string date;
+	float diem1, diem2, diem3;
+};
+
+void nhap(ThiSinh &A) {
+	getline(cin, A.name);
+	getline(cin, A.date);
+	cin >> A.diem1 >> A.diem2 >> A.diem3;
+}
+
+void in(ThiSinh A) {
+	cout << A.name << " " << A.date << " ";
+	cout << fixed << setprecision(1) << A.diem1 + A.diem2 + A.diem3;
+}
+
+int main() {
+	struct ThiSinh A;
+	nhap(A);
+	in(A);
+	return 0;
+}
