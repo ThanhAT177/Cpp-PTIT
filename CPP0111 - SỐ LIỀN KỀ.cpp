5449c2907afb9a64a8edf4a31182e807c4218e3c
Nguyễn Quốc Thành <103249850+ThanhAT177@users.noreply.github.com>
2022-10-28 00:28:08 +0700

    Create CPP0111 - SỐ LIỀN KỀ.cpp

---
+++ CPP0111 - SỐ LIỀN KỀ.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+
+std::string check(std::string s) {
+	for(int i = 0; i < s.length() - 1; ++i) {
+		if(abs(s[i + 1] - s[i]) != 1)
+			return "NO\n";
+	}
+	return "YES\n";
+}
+
+int main() {
+	std::string s;
+	int T;
+	std::cin >> T;
+	while(T--) {
+		std::cin >> s;
+		std::cout << check(s);
+	}
+	return 0;
+}
