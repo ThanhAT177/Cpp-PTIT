12c8af878a2d7eb36d6b5b8b8ac99d1e422c9b33
Nguyễn Quốc Thành <103249850+ThanhAT177@users.noreply.github.com>
2022-10-28 00:52:24 +0700

    Create CPP0413 - SẮP XẾP XEN KẼ - 1.cpp

---
+++ CPP0413 - SẮP XẾP XEN KẼ - 1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<algorithm>
+
+main() {
+	int T;
+	std::cin >> T;
+	while(T--) {
+		int n;
+		std::cin >> n;
+		int *A = new int[n];
+		for(int i = 0; i < n; ++i)
+			std::cin >> A[i];
+		std::sort(A, A + n);
+		for(int i = 0; i < n/2; ++i)
+			std::cout << A[n - i - 1] << " " << A[i] << " ";
+		if(n % 2 == 1)	std::cout << A[n/2];
+		std::cout << "\n";
+		delete []A;
+	}
+}
