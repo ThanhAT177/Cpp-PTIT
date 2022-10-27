254d0a44dd79749a82d697177ed4894d56c354a9
Nguyễn Quốc Thành <103249850+ThanhAT177@users.noreply.github.com>
2022-10-28 00:35:06 +0700

    Create CPP0130 - ƯỚC SỐ NGUYÊN TỐ.cpp

---
+++ CPP0130 - ƯỚC SỐ NGUYÊN TỐ.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cmath>
+typedef long long ll;
+
+main() {
+	int T;
+	std::cin >> T;
+	while(T--) {
+		ll N;
+		std::cin >> N;
+		while(N % 2 == 0) {
+			std::cout << 2 << " ";
+			N /= 2;
+		}
+		for(ll i = 3; i <= sqrt(N); i += 2) {
+			while(N % i == 0) {
+				N /= i;
+				std::cout << i << " ";
+			}
+		}
+		if(N > 1) std::cout << N;
+		std::cout << "\n";
+	}
+}
