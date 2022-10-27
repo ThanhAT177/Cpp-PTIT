b240bf2285b07e550a5ec5b44c25c47a3efad6bb
Nguyễn Quốc Thành <103249850+ThanhAT177@users.noreply.github.com>
2022-10-28 00:54:40 +0700

    Create CPP0422 - SỐ 0 Ở CUỐI DÃY.cpp

---
+++ CPP0422 - SỐ 0 Ở CUỐI DÃY.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+
+main() {
+	int T;
+	std::cin >> T;
+	while(T--) {
+		int n, count = 0;
+		std::cin >> n;
+		std::vector<long long> A(n);
+		for(int i = 0; i < n; ++i)
+			std::cin >> A[i];
+		for(long long x : A) {
+			if(x)	std::cout << x << " ";
+			else	++count;
+		}
+		while(count--)	std::cout << "0 ";
+		std::cout << "\n";
+		A.clear();
+	}
+}
+Footer
+© 2022 GitHub, Inc. 
