75e4b881c05c01b3b97d4848c8a921a5b89d68ad
Nguyễn Quốc Thành <103249850+ThanhAT177@users.noreply.github.com>
2022-10-28 22:14:42 +0700

    Create CPP0109 - CÂN BẰNG CHẴN LẺ.cpp

---
+++ CPP0109 - CÂN BẰNG CHẴN LẺ.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<cmath>
+
+int can_bang(int N) {
+	int ss = 0;
+	while(N) {
+		if((N % 10) % 2 == 0)
+			++ss;
+		else
+			--ss;
+		N /= 10;
+	}
+	if(ss)
+		return 0;
+	return 1;
+}
+
+main() {
+	int N, count = 0;
+	std::cin >> N;
+	for(int i = pow(10, N - 1); i < pow(10, N); ++i) {
+		if(can_bang(i)) {
+			std::cout << i << " ";
+			++count;
+			if(count == 10) {
+				count = 0;
+				std::cout << "\n";
+			}
+		}
+	}
+}
