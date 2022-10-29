1921e5cbbbfda6f7cf87647018003711659d9ec4
Nguyễn Quốc Thành <103249850+ThanhAT177@users.noreply.github.com>
2022-10-29 11:00:00 +0700

    Create CPP0125 - LIỆT KÊ SỐ NGUYÊN TỐ - 1.cpp

---
+++ CPP0125 - LIỆT KÊ SỐ NGUYÊN TỐ - 1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cmath>
+
+bool prime(int N) {
+	for(int i = 3; i <= sqrt(N); i += 2)
+		if(N % i == 0)
+			return false;
+	return true;
+}
+
+main() {
+	int a, b;
+	std::cin >> a >> b;
+	if(a > b) {
+		std::swap(a, b);
+	}
+	if(a <= 2) {
+		std::cout << "2 ";
+		++a;
+	}
+	if(a % 2 == 0)
+		++a;
+	for(int i = a; i <= b; i += 2)
+		if(prime(i) == 1)
+			std::cout << i << " ";
+}
