3f5e0c78970fda1152b736cbff7f2859110032fe
Nguyễn Quốc Thành <103249850+ThanhAT177@users.noreply.github.com>
2022-10-29 09:57:03 +0700

    Update CPP0119 - ƯỚC SỐ CHIA HẾT CHO 2.cpp

--- CPP0119 - ƯỚC SỐ CHIA HẾT CHO 2.cpp
+++ CPP0119 - ƯỚC SỐ CHIA HẾT CHO 2.cpp
@@ -10,20 +10,20 @@ main() {
 		if(N % 2)
 			std::cout << "0\n";
 		else {
-			int tmp = 0, count = 0;
+			int mu = 0, count = 0;
 			int i = 2;
 			while(N % 2 == 0) {
-				++tmp;
+				++mu;
 				N /= 2;
 			}
-			count = tmp;
+			count = mu;
 			for(int i = 3; i <= sqrt(N); i += 2) {
 				tmp = 0;
 				while(N % i == 0) {
 					N /= i;
-					++tmp;
+					++mu;
 				}
-				count *= (tmp + 1);
+				count *= (mu + 1);
 			}
 			if(N > 1)
 				count *= 2;
