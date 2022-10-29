c391f5c9434a5206ebfe3b8d55625831c116bf44
Nguyễn Quốc Thành <103249850+ThanhAT177@users.noreply.github.com>
2022-10-29 10:39:21 +0700

    Create CPP0123 - KIỂM TRA NGUYÊN TỐ.cpp

---
+++ CPP0123 - KIỂM TRA NGUYÊN TỐ.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cmath>
+
+bool prime(int N) {
+	if(N < 2)
+		return false;
+	if(N == 2)
+		return true;
+	if(N % 2 == 0)
+		return false;
+	for(int i = 3; i <= sqrt(N); i += 2)
+		if(N % i == 0)
+			return false;
+	return true;
+}
+
+main() {
+	int n;
+	std::cin >> n;
+	if(prime(n))
+		std::cout << "YES";
+	else
+		std::cout << "NO";
+}
