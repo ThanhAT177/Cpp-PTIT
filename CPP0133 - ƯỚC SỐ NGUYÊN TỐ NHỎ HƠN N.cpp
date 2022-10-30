8eba197be736ebcc39aa03539b396c5e790ede36
Nguyễn Quốc Thành <103249850+ThanhAT177@users.noreply.github.com>
2022-10-30 09:16:01 +0700

    Create CPP0133 - ƯỚC SỐ NGUYÊN TỐ NHỎ HƠN N.cpp

---
+++ CPP0133 - ƯỚC SỐ NGUYÊN TỐ NHỎ HƠN N.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<cmath>
+#include<vector>
+
+bool prime(int N) {
+	for(int i = 3; i <= sqrt(N); i += 2)
+		if(N % i == 0)
+			return false;
+	return true;
+}
+
+main() {
+	int T;
+	std::cin >> T;
+	std::vector<int> A(4, 0);
+	A[2] = 2;
+	A[3] = 3;
+	while(T--) {
+		int N;
+		std::cin >> N;
+		if(N >= A.back()) {
+			int i = A[A.size() - 1];
+			while(N >= A.back()) {
+				i += 2;
+				if(prime(i))
+					A.push_back(i);
+			}
+		}
+		for(int i = 2; A[i] <= N; ++i)
+				std::cout << A[i] << " ";
+		std::cout << "\n";
+	}
+	A.clear();
+}
