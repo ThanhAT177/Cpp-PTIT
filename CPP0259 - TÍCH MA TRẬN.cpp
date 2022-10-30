2ffceef771b9a1fbc31da6699ed218d28a083fd4
Nguyễn Quốc Thành <103249850+ThanhAT177@users.noreply.github.com>
2022-10-30 17:27:14 +0700

    Update CPP0259 - TÍCH MA TRẬN.cpp

--- CPP0259 - TÍCH MA TRẬN.cpp
+++ CPP0259 - TÍCH MA TRẬN.cpp
@@ -3,6 +3,7 @@
 main() {
 	int n, m, p;
 	std::cin >> n >> m >> p;
+	
 	int **A = new int *[n];
 	for(int i = 0; i < n; ++i)
 		A[i] = new int [m];
@@ -12,12 +13,14 @@ main() {
 	int **C = new int *[n];
 	for(int i = 0; i < n; ++i)
 		C[i] = new int [p];
+	
 	for(int i = 0; i < n; ++i)
 		for(int j = 0; j < m; ++j)
 			std::cin >> A[i][j];
 	for(int i = 0; i < m; ++i)
 		for(int j = 0; j < p; ++j)
 			std::cin >> B[i][j];
+	
 	for(int i = 0; i < n; ++i)
 		for(int j = 0; j < p; ++j) {
 			C[i][j] = 0;
