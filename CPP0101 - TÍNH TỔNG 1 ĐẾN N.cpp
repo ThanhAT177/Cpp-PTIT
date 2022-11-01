f37f6668cd68a06d98be224e20875f83ced3fb0b
Nguyễn Quốc Thành <103249850+ThanhAT177@users.noreply.github.com>
2022-11-01 10:59:16 +0700

    Update CPP0101 - TÍNH TỔNG 1 ĐẾN N.cpp

--- CPP0101 - TÍNH TỔNG 1 ĐẾN N.cpp
+++ CPP0101 - TÍNH TỔNG 1 ĐẾN N.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 
 main() {
-	int T;
-	std::cin >> T;
-	while (T--) {
-		long long N;
-		std::cin >> N;
-		std::cout << N*(N + 1)/2 << "\n";
+	int t;
+	std::cin >> t;
+	while (t--) {
+		long long n;
+		std::cin >> n;
+		std::cout << n*(n + 1)/2 << "\n";
 	}
 }
