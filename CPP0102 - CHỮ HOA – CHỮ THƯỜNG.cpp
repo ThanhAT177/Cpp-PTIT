033692b67b9364dbd0245c1400630879d62b93e5
Nguyễn Quốc Thành <103249850+ThanhAT177@users.noreply.github.com>
2022-11-01 10:58:26 +0700

    Update CPP0102 - CHỮ HOA – CHỮ THƯỜNG.cpp

--- CPP0102 - CHỮ HOA – CHỮ THƯỜNG.cpp
+++ CPP0102 - CHỮ HOA – CHỮ THƯỜNG.cpp
@@ -1,14 +1,15 @@
 #include<iostream>
 
 main() {
-	int T;
-	std::cin >> T;
-	while(T--) {
+	int t;
+	std::cin >> t;
+	while (t--) {
 		char x;
 		std::cin >> x;
-		if(x >= 'a' && x <= 'z')
+		if(x >= 'a' && x <= 'z') {
 			std::cout << char(x - 32) << "\n";
-		else if(x >= 'A' && x <= 'Z')
+		} else if (x >= 'A' && x <= 'Z') {
 			std::cout << char(x + 32) << "\n";
+		}
 	}
 }
