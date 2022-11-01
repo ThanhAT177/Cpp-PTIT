de4c3783486d714614d9310fbf268f57dd9175c3
Nguyễn Quốc Thành <103249850+ThanhAT177@users.noreply.github.com>
2022-11-01 11:19:19 +0700

    Update CPP0105 - SỐ LỘC PHÁT.cpp

--- CPP0105 - SỐ LỘC PHÁT.cpp
+++ CPP0105 - SỐ LỘC PHÁT.cpp
@@ -1,20 +1,20 @@
-#include<iostream>
-#include<string>
+#include <iostream>
 
-std::string loc_phat(std::string s) {
-	for(int i = 0; i < s.length(); ++i) {
-		if(s[i] != '0' && s[i] != '6' && s[i] != '8')
+std::string LocPhat(std::string s) {
+	for (int i = 0; i < s.length(); ++i) {
+		if (s[i] != '0' && s[i] != '6' && s[i] != '8') {
 			return "NO\n";
+		}
 	}
 	return "YES\n";
 }
 
 main() {
-	int T;
-	std::cin >> T;
-	while(T--) {
+	int t;
+	std::cin >> t;
+	while (t--) {
 		std::string s;
 		std::cin >> s;
-		std::cout << loc_phat(s);
+		std::cout << LocPhat(s);
 	}
 }
