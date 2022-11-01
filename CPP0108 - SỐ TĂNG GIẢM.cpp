2456fbc82eb58539b996fac6d870e84be26cbaca
Nguyễn Quốc Thành <103249850+ThanhAT177@users.noreply.github.com>
2022-11-01 11:39:43 +0700

    Update CPP0108 - SỐ TĂNG GIẢM.cpp

--- CPP0108 - SỐ TĂNG GIẢM.cpp
+++ CPP0108 - SỐ TĂNG GIẢM.cpp
@@ -1,54 +1,54 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
 
-int check_tang(int N) {
-	while(N >= 10) {
-		int last = N % 10;
-		N /= 10;
-		if(N % 10 >= last)
+int CheckIncrease(int n) {
+	while(n >= 10) {
+		int last = n % 10;
+		n /= 10;
+		if(n % 10 >= last)
 			return 0;
 	}
 	return 1;
 }
 
-int check_giam(int N) {
-	while(N >= 10) {
-		int last = N % 10;
-		N /= 10;
-		if(N % 10 <= last)
+int CheckDecrease(int n) {
+	while(n >= 10) {
+		int last = n % 10;
+		n /= 10;
+		if(n % 10 <= last)
 			return 0;
 	}
 	return 1;
 }
 
-int stg(int N) {
-	int last = N % 10;
-	N /= 10;
-	if(N % 10 < last)
-		return check_tang(N);
-	if(N % 10 > last)
-		return check_giam(N);
+int SoTangGiam(int n) {
+	int last = n % 10;
+	n /= 10;
+	if (n % 10 < last)
+		return CheckIncrease(n);
+	if (n % 10 > last)
+		return CheckDecrease(n);
 	return 0;
 }
 
-int prime(int N) {
-	if(!(N & 1)
+int Prime(int n) {
+	if (!(n & 1))
 		return 0;
-	for(int i = 3; i <= sqrt(N); i += 2)
-		if(N % i == 0)
+	for (int i = 3; i <= sqrt(n); i += 2)
+		if(n % i == 0)
 			return 0;        
 	return 1;
 }
 
 main() {
-	int T;
-	std::cin >> T;
-	while(T--) {
-		int N, count = 0;
-		std::cin >> N;
-		for(int i = pow(10, N - 1); i < pow(10, N); ++i)
-			if(stg(i) && prime(i))
-				count++;
+	int t;
+	std::cin >> t;
+	while(t--) {
+		int n, count = 0;
+		std::cin >> n;
+		for(int i = pow(10, n - 1); i < pow(10, n); ++i)
+			if(SoTangGiam(i) && Prime(i))
+				++count;
 		std::cout << count << "\n";
 	}
 }
