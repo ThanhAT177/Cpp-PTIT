d5763fa535db0ab784893604c0a3f3c11e332648
Nguyễn Quốc Thành <103249850+ThanhAT177@users.noreply.github.com>
2022-11-03 16:47:26 +0700

    Update CPP0135 - LIỆT KÊ SỐ CÓ BA ƯỚC SỐ.cpp

--- CPP0135 - LIỆT KÊ SỐ CÓ BA ƯỚC SỐ.cpp
+++ CPP0135 - LIỆT KÊ SỐ CÓ BA ƯỚC SỐ.cpp
@@ -1,10 +1,10 @@
-#include<iostream>
-#include<cmath>
-#include<vector>
+#include <iostream>
+#include <cmath>
+#include <vector>
 
 bool IsPrime(int n) {
-	for(int i = 3; i <= sqrt(n); i += 2)
-		if(n % i == 0)
+	for (int i = 3; i <= sqrt(n); i += 2)
+		if (n % i == 0)
 			return false;
 	return true;
 }
@@ -13,19 +13,19 @@ main() {
 	int t;
 	std::cin >> t;
 	std::vector<int> v = {4, 9};
-	while(t--) {
+	while (t--) {
 		int n;
 		std::cin >> n;
-		if(n > v.back()) {
+		if (n > v.back()) {
 			int i = sqrt(v.back()) + 2;
-			while(n > v.back()) {
+			while (n > v.back()) {
 				if (IsPrime(i)) {
 					v.emplace_back(i*i);
 				}
 				i += 2;
 			}
 		}
-		for(int i = 0; v[i] <= n; ++i) {
+		for (int i = 0; v[i] <= n; ++i) {
 			std::cout << v[i] << ' ';
 		}
 		std::cout << '\n';
