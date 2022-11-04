9886015bd5fc0162afe6c63c7028e09e6d8cf1f2
Nguyễn Quốc Thành <103249850+ThanhAT177@users.noreply.github.com>
2022-11-04 18:34:21 +0700

    Create CPP0453 - NHỎ NHẤT THỨ K.cpp

---
+++ CPP0453 - NHỎ NHẤT THỨ K.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+
+main() {
+	int t;
+	std::cin >> t;
+	while(t--) {
+		int n, k;
+		std::cin >> n >> k;
+		int a[n] = {};
+		for (int i = 0; i < n; ++i) {
+			std::cin >> a[i];
+		}
+		std::sort(a, a + n);
+		std::cout << a[k - 1] << '\n';
+	}
+}
